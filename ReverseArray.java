46f2c42cda1a05cc6fbbd05f5021f33a4aa203f8
Prajktavnimbalkar <147031604+Prajktavnimbalkar@users.noreply.github.com>
2024-03-28 09:00:45 +0530

    Add files via upload

---
+++ ReverseArray.java
@@ -0,0 +1,22 @@
+package Class18;
+
+import java.util.Arrays;
+
+public class ReverseArray {
+	
+	public static void main(String[] args) {
+		
+		int[] array = {1,2,3,4,5};
+		int length = array.length;
+		for (int i = 0;i<length/2;++i){
+			
+			int temp = array[i];
+			
+			array[i] = array[length-1-i];
+			array [length-1-i] = temp;
+			
+		}
+		System.out.println("Reversed array = "+ Arrays.toString(array));
+	}
+
+}
