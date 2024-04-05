59d53121f192ceaf6a5ed198bc0c8297302d1394
Prajktavnimbalkar <147031604+Prajktavnimbalkar@users.noreply.github.com>
2024-04-05 09:16:50 +0530

    Add files via upload

---
+++ PrimeChecker.java
@@ -0,0 +1,33 @@
+package Class18;
+import java.util.Scanner;
+
+	public class PrimeChecker {
+	    public static boolean isPrime(int number) {
+	        if (number <= 1) {
+	            return false;
+	        }
+	        for (int i = 2; i <= Math.sqrt(number); i++) {
+	            if (number % i == 0) {
+	                return false;
+	            }
+	        }
+	        return true;
+	    }
+
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+
+	        System.out.print("Enter a number: ");
+	        int userInput = scanner.nextInt();
+
+	        if (isPrime(userInput)) {
+	            System.out.println(userInput + " is a prime number.");
+	        } else {
+	            System.out.println(userInput + " is not a prime number.");
+	        }
+
+	        scanner.close();
+	    }
+	}
+
+
